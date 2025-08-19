3840a6cebc1c9ec1294c11e78e70fc6049e7cb24
yannicknqdev <yannicknq.dev@gmail.com>
2025-08-19 10:49:27 -0500

    Implement parsing for ExchangeRateResponse

---
+++ src/main/java/com/alura/coinfluxcraft/parser/JsonParser.java
@@ -0,0 +1,34 @@
+package main.java.com.alura.coinfluxcraft.parser;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import main.java.com.alura.coinfluxcraft.models.ExchangeRateResponse;
+
+public class JsonParser {
+    private static final Gson gson = new Gson();
+
+    public static ExchangeRateResponse parseExchangeRateResponse(String jsonResponse) {
+        try {
+            return gson.fromJson(jsonResponse, ExchangeRateResponse.class);
+        } catch (JsonSyntaxException e) {
+            throw new RuntimeException("Error parsing JSON response: " + e.getMessage(), e);
+        }
+    }
+
+    public static String toJson(Object object) {
+        try {
+            return gson.toJson(object);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting object to JSON: " + e.getMessage(), e);
+        }
+    }
+
+    public static boolean isValidJson(String jsonString) {
+        try {
+            gson.fromJson(jsonString, Object.class);
+            return true;
+        } catch (JsonSyntaxException e) {
+            return false;
+        }
+    }
+}
